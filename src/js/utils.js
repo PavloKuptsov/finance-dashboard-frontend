dc8aa2969bf599d5f5458629f91f0a3da38c31a3
Pavlo Kuptsov <pavlo.kupstov@sigma.software>
2025-02-20 00:51:17 +0200

    Fixed color parsing

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,6 @@
 export const defaultCurrencyCode = 'UAH';
 const currencySymbols = {'UAH': '₴', 'USD': '$', 'EUR': '€'};
-export const apiUrl = 'http://127.0.0.1:8000';
+export const apiUrl = 'http://192.168.1.3:8000';
 
 export const tooltipOptions = {
     backgroundColor: '#f5f5f5',
